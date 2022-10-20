e8750380ae5c013c41978f31416e24c89ed84234
Darwin Esteban Cifuentes G <tecnicoesteban01@gmail.com>
2022-10-20 16:58:01 -0500

    hola mundo creado

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>Hola mundo</Text>
       <StatusBar style="auto" />
+      <Text>
+        U2. Actividad de aprendizaje para RA: Actividad 4. Configurando un IDE y
+        Github - App Hola Mundo
+      </Text>
     </View>
   );
 }
@@ -13,8 +17,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  texto: {
+    fontSize: "40rem",
+    fontWeight: "700",
+    color: "dodgerblue",
+    borderStyle: "solid",
+    borderColor: "dodgerblue",
+    borderWidth: 4,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    borderRadius: 20,
+    marginBottom: 50,
   },
 });
